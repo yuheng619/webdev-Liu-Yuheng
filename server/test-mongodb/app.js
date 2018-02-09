f07bc12b44827badec5ef783d9dabf5a19d470ae
liuyuheng <yuheng@ccis.neu.edu>
2018-02-08 20:56:02 -0800

    update app.js

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -13,7 +13,7 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // use yours
+    connectionString += 'ds229878.mlab.com:29878/heroku_5czr6h5n'; // use yours
   }
 
   var mongoose = require("mongoose");
